971c2164377553db71b533ef74dbe14af4089088
Om Pant <ompant992@gmail.com>
2024-04-20 13:26:45 +0530

    Created apiError and ApiResponse clas utilities

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+	.then(() => {
+		app.on("error", (error) => {
+			console.log("Something Unexpected Happened, Error:: ", error);
+			throw error;
+		});
+		app.listen(process.env.PORT || 3000, () => {
+			console.log(`Server Running at Port  ${process.env.PORT || 3000}`);
+		});
+	})
+	.catch((err) => {
+		console.log("DB Connection Failed :: Error -> ", err);
+		process.exit(1);
+	});
